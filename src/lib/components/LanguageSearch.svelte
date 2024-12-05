01fdee1deac0e6fc3e808c1e83c5bfe016ddf787
chejel <chejenj@gmail.com>
2024-12-05 11:40:57 -0500

    Modify helper text

--- src/lib/components/LanguageSearch.svelte
+++ src/lib/components/LanguageSearch.svelte
@@ -33,41 +33,30 @@
 >
 `;
 
-	$: languageHeader =
-		// $selectedLanguage
-		// 	? languageList.length > 1
-		// 		? languageList.length - value.length === 0
-		// 			? `<span class="filter-field-label" style="color: var(--gray);">${languageIcon} No additional languages available</span>`
-		// 			: `
-		// 				<span class="filter-field-label">
-		// 					${languageIcon} Search by primary languages <span style="font-weight: 400;">(${languageList.length - value.length} more)</span></span>
-		// 				<p class="filter-field-description">
-		// 					Selecting more than 1 language will show outlets that have content in all of those languages.
-		// 				</p>
-		// 			  `
-		// 		: `<span class="filter-field-label" style="color: var(--gray);">${languageIcon} No additional languages available</span>`
-		// 	:
-		$selectedLanguage
-			? languageList.length > 1
-				? languageList.length - value.length === 0
-					? `<span class="filter-field-label">${languageIcon} Search by primary language <span style="font-weight: 400;">(No more options)</span></span>
+	$: languageHeader = $selectedLanguage
+		? languageList.length > 1
+			? languageList.length - value.length === 0
+				? `<span class="filter-field-label">${languageIcon} Search by primary language <span style="font-weight: 400;">(No more options)</span></span>
 					<p class="filter-field-description">
 							Map shows ${filteredDirectory.length > 1 ? 'outlets that have' : 'the outlet that has'} content in the ${value.length} selected languages.
 						</p>`
-					: `
+				: `
 						<span class="filter-field-label">
-							${languageIcon} Search by primary language <span style="font-weight: 400;">(${languageList.length - value.length} more options)</span></span>
+							${languageIcon} Search by primary language <span style="font-weight: 400;">(${languageList.length - value.length} more ${languageList.length - value.length > 1 ? 'options' : 'option'})</span></span>
 						<p class="filter-field-description">
 							Selecting more than 1 language will show outlets that have content in all of those languages.
 						</p>
 					  `
-				: `<span class="filter-field-label">
-							${languageIcon} Search by primary language <span style="font-weight: 400;">(${languageList.length - value.length} more options)</span></span>
+			: `<span class="filter-field-label">
+							${languageIcon} Search by primary language 
+							
+							</span>
 						<p class="filter-field-description">
 							No additional languages available.
 						</p>`
-			: //`<span class="filter-field-label" style="color: var(--gray);">${languageIcon} No additional languages available</span>`
-				`
+		: // <span style="font-weight: 400;">(${languageList.length - value.length} more options)</span>
+			//`<span class="filter-field-label" style="color: var(--gray);">${languageIcon} No additional languages available</span>`
+			`
 			<span class="filter-field-label">${languageIcon} Search by primary language <span style="font-weight: 400;">(${languageList.length} options)</span></span>
 		  `;
 
