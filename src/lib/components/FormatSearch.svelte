3def4d58a050b11eb0bb50edc8769e020c31ac53
chejel <chejenj@gmail.com>
2024-12-05 11:42:28 -0500

    Clean up code

--- src/lib/components/FormatSearch.svelte
+++ src/lib/components/FormatSearch.svelte
@@ -44,25 +44,6 @@
 	let formatHeader;
 	const formatIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="15px" viewBox="0 -960 960 960" width="15px" fill="var(--format-color)"><path d="M160-80q-17 0-28.5-11.5T120-120v-558q0-15 6-25.5t20-16.5l400-160q20-8 37 5.5t17 34.5v120h40q17 0 28.5 11.5T680-680v120h-80v-80H200v480h207l80 80H160Zm200-640h160v-62l-160 62ZM680-80q-83 0-141.5-58.5T480-280q0-83 58.5-141.5T680-480q83 0 141.5 58.5T880-280q0 83-58.5 141.5T680-80Zm-50-100 160-100-160-100v200Zm-430 20v-480 480Z"/></svg>`;
 
-	// $: formatHeader = $selectedFormat
-	// 	? `<span class="filter-field-label" style="color: var(--gray);">
-	// 				${formatIcon}
-	// 				No additional formats can be selected
-	// 			</span>
-	// 			<p class="filter-field-description">
-	// 				To choose another format, change the selection.
-	// 			</p>
-	// 			`
-	// 	: `<span class="filter-field-label">${formatIcon} Search by primary format <span style="font-weight: 400;">(${formatList.length} total)</span></span>`;
-
-	// $: formatHeader = $selectedFormat
-	// 	? `<span class="filter-field-label">${formatIcon} Search by primary format <span style="font-weight: 400;">(${formatList.length} total)</span></span>
-	// 			<p class="filter-field-description">
-	// 				To choose another format, change the selection.
-	// 			</p>
-	// 			`
-	// 	: `<span class="filter-field-label">${formatIcon} Search by primary format <span style="font-weight: 400;">(${formatList.length} total)</span></span>`;
-
 	$: formatHeader = $selectedFormat
 		? `<span class="filter-field-label">${formatIcon} Search by primary format </span>
 				<p class="filter-field-description">
@@ -72,12 +53,6 @@
 		: `<span class="filter-field-label">${formatIcon} Search by primary format <span style="font-weight: 400;">(${formatList.length} options)</span></span><p class="filter-field-description">
 					Only one format can be selected at a time.
 				</p>`;
-
-	// $: formatHeader = `<span class="filter-field-label">${formatIcon} Search by primary format <span style="font-weight: 400;">(${formatList.length} total)</span></span>
-	// 			<p class="filter-field-description">
-	// 				Only one format can be selected at a time.
-	// 			</p>
-	// 			`;
 
 	// Clear/reset selected filter when outlet is selected
 	$: if ($selectedOutlet) {
